ecd65659137d8e370c9510db1cef8615d9f33c3d
Ori0li <gogo5125@naver.com>
2025-04-16 11:08:47 +0900

    에러메세지 추가

--- routers/productRoute.js
+++ routers/productRoute.js
@@ -6,13 +6,17 @@ const { validator } = require("../middleware/validator.js");
 
 productRouter.get("/", async (req, res) => {
   const products = await productService.getAll();
-  res.json(products);
+  res.status(200).json(products);
+  res.status(400).json({
+    success: false,
+    message: "전체 상품 조회 실패",
+  });
 });
 
 productRouter.get("/:name", existor, async (req, res) => {
   const { name } = req.params;
   await productService.getProduct(name);
-  res.json({
+  res.status(200).json({
     success: true,
     message: `${name} 상품 조회 성공`,
     data: { product: req.product },
@@ -22,19 +26,27 @@ productRouter.get("/:name", existor, async (req, res) => {
 productRouter.post("/", async (req, res) => {
   const { name, price } = req.body;
   await productService.createProduct(name, price);
-  res.json({ success: true, message: `${name} 상품 추가 성공` });
+  res.status(200).json({ success: true, message: `${name} 상품 추가 성공` });
+  res.status(400).json({
+    success: false,
+    message: `${name} 상품 추가 실패`,
+  });
 });
 
 productRouter.put("/", validator, async (req, res) => {
   const { id, name, price } = req.body;
   await productService.updateProduct(id, name, price);
-  res.json({ success: true, message: `${name} 상품 수정 완료` });
+  res.status(200).json({ success: true, message: `${name} 상품 수정 완료` });
 });
 
 productRouter.delete("/", async (req, res) => {
   const { id } = req.body;
   await productService.deleteProduct(id);
-  res.json({ success: true, message: "상품 삭제 완료" });
+  res.status(200).json({ success: true, message: "상품 삭제 완료" });
+  res.status(400).json({
+    success: false,
+    message: "상품 삭제 실패",
+  });
 });
 
 module.exports = { productRouter };
